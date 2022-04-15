be627b6f7997d30da6bdd53eebd90d063b5d9613
tariel36 <2964246+tariel36@users.noreply.github.com>
2022-04-15 12:54:51 +0200

    Build script

--- build/Build.cs
+++ build/Build.cs
@@ -9,6 +9,7 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
@@ -43,73 +44,214 @@ public class Build
     Target Clean => _ => _
         .Executes(() =>
         {
+            CleanDeploy();
         });
 
     Target Prepare => _ => _
         .DependsOn(Clean)
         .Executes(() =>
         {
-            ToBuild = CsSolution.AllProjects.Where(x => !x.Directory.Contains("\\Internal\\")).ToArray();
+            ToBuild = CsSolution.AllProjects.Where(x => !x.Directory.ToString().Contains("\\Internal\\")).ToArray();
+            EnsureGeneratePackageOnBuild();
+            EnsureLicense();
         });
 
     Target Restore => _ => _
         .DependsOn(Prepare)
         .Executes(() =>
         {
+            RestoreProjects();
         });
 
-    Target EnsureGeneratePackageOnBuild => _ => _
+    Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
-            foreach (Project prj in ToBuild)
+            CompileProjects();
+        });
+
+    Target DeployLocal => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            MoveNuggetPackages();
+        });
+
+    Target DeployWeb => _ => _
+        .DependsOn(DeployLocal)
+        .Executes(() =>
+        {
+            DeployNuggetPackages();
+        });
+
+    private void CleanDeploy()
+    {
+        Paths paths = GetPaths();
+
+        if (Directory.Exists(paths.DeployDirecotry))
+        {
+            Directory.Delete(paths.DeployDirecotry, true);
+        }
+    }
+
+    private void MoveNuggetPackages()
+    {
+        Paths paths = GetPaths();
+
+        if (!Directory.Exists(paths.DeployDirecotry))
+        {
+            Directory.CreateDirectory(paths.DeployDirecotry);
+        }
+
+        foreach (string nuPkgFilePath in Directory.EnumerateFiles(paths.CsDirectory, "*.nupkg", SearchOption.AllDirectories).Where(x => x.Contains($"\\{Configuration}\\")))
+        {
+            string targetFilePath = Path.Combine(paths.DeployDirecotry, Path.GetFileName(nuPkgFilePath));
+
+            File.Copy(nuPkgFilePath, targetFilePath);
+        }
+    }
+
+    private void RestoreProjects()
+    {
+        MSBuildTasks.MSBuild(s => s
+            .SetTargetPath(CsSolution)
+            .SetConfiguration(Configuration)
+            .SetTargets("Restore")
+            .SetMaxCpuCount(Environment.ProcessorCount)
+            .SetNodeReuse(IsLocalBuild)
+        );
+    }
+
+    private void CompileProjects()
+    {
+        MSBuildTasks.MSBuild(s => s
+            .SetTargetPath(CsSolution)
+            .SetConfiguration(Configuration)
+            .SetTargets("Rebuild")
+            .SetMaxCpuCount(Environment.ProcessorCount)
+            .SetNodeReuse(IsLocalBuild)
+        );
+    }
+
+    private void DeployNuggetPackages()
+    {
+
+    }
+
+    private void EnsureLicense()
+    {
+        const string LicensePathLine = @"<None Include=""pathToLicense/LICENSE"" Pack=""true"" Visible=""false"" PackagePath="""" />";
+        const string LicenseProperties = @"
+            <PropertyGroup>
+                <PackageLicenseFile>LICENSE</PackageLicenseFile>
+            </PropertyGroup>
+
+            <ItemGroup>
+                "+LicensePathLine+@"
+            </ItemGroup>
+        ";
+
+        Paths paths = GetPaths();
+
+        foreach (Project prj in ToBuild)
+        {
+            string pathToLicense = GetRelativePath(paths.RootDirectory, prj.Path);
+
+            List<string> fileLines = File.ReadAllLines(prj.Path).ToList();
+
+            if (fileLines.All(x => !x.Contains("PackageLicenseFile")))
             {
-                if (string.IsNullOrWhiteSpace(prj.GetProperty("Deterministic")))
+                string license = LicenseProperties.Replace(nameof(pathToLicense), pathToLicense);
+
+                fileLines.Insert(fileLines.IndexOf(fileLines.Find(x => x.Contains("</Project>"))), license);
+
+                File.WriteAllLines(prj.Path, fileLines.ToArray());
+            }
+            else
+            {
+                string license = LicensePathLine.Replace(nameof(pathToLicense), pathToLicense);
+                string line = fileLines.Find(x => x.EndsWith(@"LICENSE"" Pack=""true"" PackagePath="""" />"));
+
+                if (!line.Contains(license))
                 {
-                    throw new InvalidOperationException($"Every project must have `Deterministic` defined. Project: `{prj.Name}`.");
-                }
-
-                List<string> fileLines = File.ReadAllLines(prj.Path).ToList();
-
-                if (fileLines.Select(x => x.Trim()).Contains("<GeneratePackageOnBuild>false</GeneratePackageOnBuild>"))
-                {
-                    int packageIdx = fileLines.Select(x => x.Trim()).ToList().IndexOf("<GeneratePackageOnBuild>false</GeneratePackageOnBuild>");
-
-                    if (packageIdx < 0)
-                    {
-                        throw new InvalidOperationException($"Failed to find `GeneratePackageOnBuild`. Project: `{prj.Name}`.");
-                    }
-
-                    fileLines[packageIdx] = "<GeneratePackageOnBuild>true</GeneratePackageOnBuild>";
-
-                    File.WriteAllLines(prj.Path, fileLines.ToArray());
-                }
-                else if (!fileLines.Select(x => x.Trim()).Contains("<GeneratePackageOnBuild>true</GeneratePackageOnBuild>"))
-                {
-                    int deterministicIdx = fileLines.Select(x => x.Trim()).ToList().IndexOf("<Deterministic>true</Deterministic>");
-                    if (deterministicIdx < 0)
-                    {
-                        deterministicIdx = fileLines.Select(x => x.Trim()).ToList().IndexOf("<Deterministic>false</Deterministic>");
-                    }
-
-                    if (deterministicIdx < 0)
-                    {
-                        throw new InvalidOperationException($"Failed to find `Deterministic`. Project: `{prj.Name}`.");
-                    }
-
-                    fileLines.Insert(deterministicIdx, "<GeneratePackageOnBuild>true</GeneratePackageOnBuild>");
+                    int idx = fileLines.IndexOf(line);
+                    fileLines[idx] = license;
 
                     File.WriteAllLines(prj.Path, fileLines.ToArray());
                 }
             }
-        });
+        }
+    }
 
-    Target Compile => _ => _
-        .DependsOn(EnsureGeneratePackageOnBuild)
-        .Executes(() =>
+    private void EnsureGeneratePackageOnBuild()
+    {
+        foreach (Project prj in ToBuild)
         {
-            Console.WriteLine(string.Join("\r\n", CsSolution.AllProjects.Select(x => x.Name)));
-            Console.ReadKey();
-        });
+            if (string.IsNullOrWhiteSpace(prj.GetProperty("Deterministic")))
+            {
+                throw new InvalidOperationException($"Every project must have `Deterministic` defined. Project: `{prj.Name}`.");
+            }
 
+            List<string> fileLines = File.ReadAllLines(prj.Path).ToList();
+
+            if (fileLines.Select(x => x.Trim()).Contains("<GeneratePackageOnBuild>false</GeneratePackageOnBuild>"))
+            {
+                int packageIdx = fileLines.Select(x => x.Trim()).ToList().IndexOf("<GeneratePackageOnBuild>false</GeneratePackageOnBuild>");
+
+                if (packageIdx < 0)
+                {
+                    throw new InvalidOperationException($"Failed to find `GeneratePackageOnBuild`. Project: `{prj.Name}`.");
+                }
+
+                fileLines[packageIdx] = "<GeneratePackageOnBuild>true</GeneratePackageOnBuild>";
+
+                File.WriteAllLines(prj.Path, fileLines.ToArray());
+            }
+            else if (!fileLines.Select(x => x.Trim()).Contains("<GeneratePackageOnBuild>true</GeneratePackageOnBuild>"))
+            {
+                int deterministicIdx = fileLines.Select(x => x.Trim()).ToList().IndexOf("<Deterministic>true</Deterministic>");
+                if (deterministicIdx < 0)
+                {
+                    deterministicIdx = fileLines.Select(x => x.Trim()).ToList().IndexOf("<Deterministic>false</Deterministic>");
+                }
+
+                if (deterministicIdx < 0)
+                {
+                    throw new InvalidOperationException($"Failed to find `Deterministic`. Project: `{prj.Name}`.");
+                }
+
+                fileLines.Insert(deterministicIdx, "<GeneratePackageOnBuild>true</GeneratePackageOnBuild>");
+
+                File.WriteAllLines(prj.Path, fileLines.ToArray());
+            }
+        }
+    }
+
+
+    private Paths GetPaths()
+    {
+        string rootDirectory = Path.GetFullPath($"{CsSolution.Directory}/../..");
+        string deployDirectory = Path.Combine(rootDirectory, Paths.RelativeArtifactsDirectory);
+
+        return new Paths
+        {
+            RootDirectory = rootDirectory,
+            DeployDirecotry = deployDirectory,
+            CsDirectory = Path.Combine(rootDirectory, "CS")
+        };
+    }
+
+    private string GetRelativePath(string toPath, string fromPath)
+    {
+        string remainingPath = fromPath.Replace(toPath, string.Empty);
+        return string.Join("/", remainingPath.Split('\\').Skip(2).Select(x => ".."));
+    }
+
+    private class Paths
+    {
+        public const string RelativeArtifactsDirectory = "local/deploy";
+        public string RootDirectory;
+        public string DeployDirecotry;
+        public string CsDirectory;
+    }
 }
